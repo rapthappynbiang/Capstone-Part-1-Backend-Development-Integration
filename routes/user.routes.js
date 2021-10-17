7098bee141ccd1807f5856ec19c9b815ed1522b5
Pynbiangbrlang Lyngwa Rapthap <rapthappynbiang@gmail.com>
2021-10-17 06:33:19 -0800

    REST API Endpoint-1 Signup and login

---
+++ routes/user.routes.js
@@ -0,0 +1,16 @@
+module.exports = app=>{
+    //import express router
+    const router = require('express').Router();
+    //import the controller
+    const user = require('../controllers/user.controller.js');
+
+    //POST /auth/signup
+    router.post('/auth/signup', user.signUp);
+
+    //POST /auth/login
+    router.post('/auth/login', user.login);
+
+
+    //use the router
+    app.use('/api', router);
+}
