13cfb27b52bcb5ece6b12ed26b7e9ea840959254
Pynbiangbrlang Lyngwa Rapthap <rapthappynbiang@gmail.com>
2021-10-18 08:11:40 -0800

    REST API Endpoint-2 /address

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,36 +1,18 @@
-require('dotenv').config();
 const db = require('../models');
-const mongoose = require('mongoose');
+const Users = db.users;
 const httpStatus = require('http-status-codes');
 const bcrypt = require('bcrypt');
 const {atob} = require('b2a');
-const jwt = require('jsonwebtoken');
-const secret = "my secret";
-const Users = db.users;
+const {generateAccessToken} = require('../middleware/auth');
+const {validateEmail, validateContact} = require('../middleware/validation');
 
-//function to validate the email return true if valid and false otherwise
-const validateEmail = (email)=>{
-
-    //regex for validating email
-    const regx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-z]{2,6})/
-    var result = regx.test(email);
-    return result;
-}
-
-//function to validate the user's phone number
-const validateContact = (contact)=>{
-
-    //regex for validating phone number
-    const regex = /^(\+\d{1,3}[- ]?)?\d{10}$/
-    return regex.test(contact);
-
-}
 
 exports.signUp = (req, res)=>{
 
     //validate the email
     var isEmailValid = validateEmail(req.body.email);
     if(!isEmailValid){
+        console.log("Email is invalid")
         res.setHeader('Content-Type','application/json')
         res.status(httpStatus.StatusCodes.BAD_REQUEST).json({message: "Invalid email-id format!"}).end();
         return;
@@ -38,13 +20,14 @@ exports.signUp = (req, res)=>{
 
     //validate phone number
     if(!validateContact(req.body.contactNumber)){
+        console.log("Contact is not valid")
         res.setHeader('Content-Type','application/json')
         res.status(httpStatus.StatusCodes.BAD_REQUEST).json({message: "Invalid contact number!"}).end();
         return;     
     }
      
     //find user by email if user is there then say user exists send 
-    Users.find({email: req.email}).then(result=>{
+    Users.find({email: req.body.email}).then(result=>{
         if(result.length != 0){
             res.setHeader('Content-Type', 'application/json');
             res.status(httpStatus.StatusCodes.FORBIDDEN).json({message: "Try any other email, this email is already registered!"}).end();
@@ -53,13 +36,13 @@ exports.signUp = (req, res)=>{
 
         //else insert user to data base 
         const data ={
-           email: req.body.email,
-           first_name: req.body.first_name,
-           last_name: req.body.last_name,
-           password:  req.body.password,
-           contact:  req.body.contactNumber,
-           createdAt:  Date.now(),
-           updatedAt:  Date.now()
+            email: req.body.email,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            contact:  req.body.contactNumber,
+            password:  req.body.password,
+            createdAt:  Date.now(),
+            updatedAt:  Date.now()
         }
 
         //check if role Admin is provided
@@ -72,12 +55,6 @@ exports.signUp = (req, res)=>{
         .then(hashPassword=>{
             data.password = hashPassword;
 
-            //count users already in db
-            Users.countDocuments({}).then(count=>{
-
-                //assign user id as count++
-                data.id = ++count;
-
                 //insert data to db
                 Users.insertMany([data])
                 .then(userData=>{
@@ -86,33 +63,34 @@ exports.signUp = (req, res)=>{
                 })
                 .catch(err=>{
                     //if error occurs while saving user to db
+                    console.log(err);
                     res.setHeader('Content-Type', 'application/json');
-                    res.status(httpStatus.StatusCodes.BAD_REQUEST).json().end();
-                });
-
-           })   
+                    res.status(httpStatus.StatusCodes.BAD_REQUEST).json({message: "error while inserting"}).end();
+                });  
         })
         .catch(err=>{
             //handle error while encrypting password
+            console.log(err);
              res.setHeader('Content-Type', 'applicatin/json');
-             res.status(httpStatus.StatusCodes.BAD_REQUEST).json().end();
+             res.status(httpStatus.StatusCodes.BAD_REQUEST).json({"message": "error encryptin password"}).end();
         })
         
     })
     .catch(err=>{
         //handle error while finding user with email if any
+        console.log(err);
         res.setHeader('Content-Type', 'applicatin/json');
-        res.status(httpStatus.StatusCodes.BAD_REQUEST).json().end();
+        res.status(httpStatus.StatusCodes.BAD_REQUEST).json({"message": "This is the error"}).end();
     })
 }
 
 // POST /auth/login
 exports.login = (req, res)=>{
-     console.log(req.body.password);
+     
     //decrypt th encoded password from b2a
     var password = atob(req.body.password);
 
-
+   //find the user with the given email
     Users.find({email: req.body.email})
     .then(response=>{
        if(response.length == 0){
@@ -135,16 +113,15 @@ exports.login = (req, res)=>{
             }else{
                  //password entered match correctly
                 //generate token
-                //define the payload as user.email
-                var user = {
-                    email: response[0].email
-                }
-                var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+                //define the payload as user data in the response
+                var user = response[0].toJSON();
+                
+                var token = generateAccessToken(user);
                 
                 //set the response header x-auth-token 
                 res.setHeader('Content-Type', 'application/json');
                 res.setHeader('x-auth-token', token);
-                res.status(200)
+                res.status(httpStatus.StatusCodes.OK)
                 .json({
                    email: response[0].email,
                    name: response[0].first_name+" "+response[0].last_name,
--- test/index.test.js
+++ test/index.test.js
@@ -2,17 +2,36 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
 const {btoa} = require('b2a');
-const app = require('../index.js');
+//getting the express app
+const app = require('../app');
+//importing the db object and making connection
+const db = require('../models');
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 chai.should();
 chai.use(chaiHttp);
 
+var accessToken ="";
+
 describe("Testing CRUD Apis for upGrad-E-Shop", ()=>{
 
     //testing out the user related apis
-    describe("Testing Auth Apis", ()=>{
+    describe("Testing /auth Apis", ()=>{
 
-        //testing the / api
+        //testing the / api just for testing sake
         it("It should get status status OK when hitting /", (done)=>{
             chai.request(app)
             .get('/')
@@ -21,32 +40,34 @@ describe("Testing CRUD Apis for upGrad-E-Shop", ()=>{
                expect(err).to.be.a('null');
                expect(res).to.have.status(200);
                expect(res.body).to.be.a('object');
+               done();
             })
-            done(); 
+            
         })
 
-        //testing /api/auth/signup api
-        it("It should insert user to db on hitting /api/auth/signup", (done)=>{
+       //testing /api/auth/signup api
+        it("It should insert user to db on hitting /api/auth/signup endpoint", (done)=>{
             chai.request(app)
             .post('/api/auth/signup')
             .send({
-                "password": "password",
-                "first_name": "ishwar",
-                "last_name": "soni",
-                "email": "ishwar.soni@upgrad.com",
-                "contactNumber": 1234567890
+                email : "ishwar.soni@upgrad.com",
+                password: "password",
+                first_name: "ishwar",
+                last_name: "soni",
+                contactNumber: "1234567890"
             })
             .end((err,res)=>{
                expect(err).to.be.a('null');
                expect(res).to.have.status(200);
                expect(res.body).to.be.a('object');
+               done();
             });
-            done();
         })
 
         //testing login /api/auth/login
-        it("It should login when we hit POST /api/auth/login", (done)=>{
-            chai
+        it("It should log in the user when we hit POST /api/auth/login endpoint", (done)=>{
+            
+         chai
             .request(app)
             .post('/api/auth/login')
             .send({
@@ -57,6 +78,33 @@ describe("Testing CRUD Apis for upGrad-E-Shop", ()=>{
                 expect(err).to.be.a('null');
                 expect(res).to.have.header('x-auth-token');
                 expect(res.header["x-auth-token"]).to.be.a('String');
+                expect(res.body).to.be.a('object');
+            })
+            done();
+        })
+     })
+
+
+    //Testing POST /address api  
+    describe("Testing the POST /address  api", ()=>{
+        it('it should not post the given address to db when hitting POST /api/address endpoint', (done)=>{
+            console.log(accessToken)
+            chai.request(app)
+            .post('/api/address')
+            //please give your own authorization key while testing by loggin first
+            .set('Authorization', 'Bearer '+accessToken)
+            .send({
+                  "name": "ishwar soni",
+                  "city": "Kolkata",
+                  "state": "West Bengal",
+                  "street": "503-2",
+                  "contactNumber": "1234567890",
+                  "landmark": "",
+                  "zipCode": "700106"
+            })
+            .end((err,res)=>{
+                expect(err).to.be.a('null');
+                expect(res).to.have.status(401);
                 expect(res.body).to.be.a('object');
             })
             done();
